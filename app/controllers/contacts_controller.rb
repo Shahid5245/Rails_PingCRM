aa80c73d2a16f975ae62ac7c4a65876ac0994abe
Shahid5245 <mohamedshahid5245@gmail.com>
2025-01-02 17:44:40 +0530

    Basic view and controller functionality added

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,53 @@
 class ContactsController < ApplicationController
+  before_action :set_organizations, only: [:new, :edit]
 
   def index
     @contacts = Contact.all
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+    @organizations = Organization.all
+  end
+
+  def create
+    @contact = Contact.create!(contact_params_permit)
+    if @contact
+      redirect_to @contact
+    end
+  end
+
+  def edit
+    @contact = Contact.find_by(id: params[:id])
+    @organization = Organization.all
+  end
+
+  def update
+    contact = Contact.find_by(id: params[:id])
+    if contact.update(contact_params_permit)
+      redirect_to contact
+    end
+  end
+
+  def destroy
+    contact = Contact.find_by(id: params[:id])
+    if contact
+      contact.destroy
+      redirect_to contacts_path
+    end
+  end
+
+  private
+
+  def set_organizations
+    @organizations = Organization.all
+  end
+
+  def contact_params_permit
+    params.require(:contact).permit(:first_name, :last_name, :organization_id, :email, :phone, :city, :address, :region, :country, :postal_code)
   end
 end
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,4 +3,44 @@ class OrganizationsController < ApplicationController
   def index
     @organizations = Organization.all
   end
+
+  def show
+    @organization = Organization.find_by(id: params[:id])
+  end
+
+  def new
+    @organization = Organization.new
+  end
+
+  def create
+    @organization = Organization.create!(org_params_permit)
+    if @organization
+      redirect_to @organization
+    end
+  end
+
+  def edit
+    @organization = Organization.find_by(id: params[:id])
+  end
+
+  def update
+    organization = Organization.find_by(id: params[:id])
+    if organization.update(org_params_permit)
+      redirect_to organization
+    end
+  end
+
+  def destroy
+    organization = Organization.find_by(id: params[:id])
+    if organization
+      organization.destroy
+      redirect_to organizations_path
+    end
+  end
+
+  private
+
+  def org_params_permit
+    params.require(:organization).permit(:name, :email, :phone, :city, :address, :region, :country, :postal_code)
+  end
 end
